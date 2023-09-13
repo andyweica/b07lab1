64877d6a1de38b462c505b8c23925a94fd26230f
Andy Wei <85173924+andyweica@users.noreply.github.com>
2023-09-13 14:54:32 -0400

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,3 +1,11 @@
-public class polynomial{
+public class Polynomial{
+    double[]coefficients;
+  
+    public Polynomial(){
+      double[] coefficients={0};
+    }
 
+    public Polynomial(double[]){
+      
+    }
 }
